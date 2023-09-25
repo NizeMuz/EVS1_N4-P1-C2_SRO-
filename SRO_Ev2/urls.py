1941818b73cabf0264b2caac5429f2c40d2283ed
Alex <alex.oporto04@inacapmail.cl>
2023-09-25 15:16:11 -0300

    segunda rama lista

--- SRO_Ev2/urls.py
+++ SRO_Ev2/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from SRO_app2 import views as v2
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('vista2/', v2.Vista),
